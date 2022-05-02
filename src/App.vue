e61ea43a78dfcb2181a29c2e8a8fcd0d4e718a19
AntonioSabella <asabella6@gmail.com>
2022-05-02 18:08:25 +0200

    Initial commit vue-cli-first

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TitlePro />
+    <HeroJumbo />
+  </div>
+</template>
+
+<script>
+import TitlePro from './components/TitlePro.vue'
+import HeroJumbo from './components/HeroJumbo.vue'
+export default {
+  name: 'App',
+  components: {
+    TitlePro,
+    HeroJumbo
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HeroJumbo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="hello">
+        <div class="container">
+            <h2>Io sono un Jumbotron</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab obcaecati nisi repellendus! Voluptas, aliquam mollitia.</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HeroJumbo',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h2 {
+    background-color: cornflowerblue;
+}
+
+.container {
+    width: 1000px;
+    height: 500px;
+    background-image: url(https://picsum.photos/1000/500);
+    background-repeat: no-repeat;
+    border-radius: 0.5rem;
+    margin: auto;
+    position: relative;
+}
+
+p {
+    color: white;
+    position: absolute;
+}
+</style>
